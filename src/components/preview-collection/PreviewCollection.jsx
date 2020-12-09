db6983154eba75d0f0404953831e5f1723136cd1
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-09 14:18:02 -0600

    updating app with shop page collection prev comp and collection item comp

--- src/components/preview-collection/PreviewCollection.jsx
+++ src/components/preview-collection/PreviewCollection.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
+import CollectionItem from '../collection-item/CollectionItem'
+
 import './PreviewCollection.styles.scss'
 
 const PreviewCollection = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview"></div>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))
-      }
-
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...itemProps }) => (
+            <CollectionItem key={id} {...itemProps} />
+          ))}
+      </div>
     </div>
   )
 }
