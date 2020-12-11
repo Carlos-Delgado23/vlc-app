22a818e6d4267ddee548b96cb9338961914c6b93
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-12-11 15:00:16 -0600

    style for sign up

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -8,8 +8,8 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
 import './SignUp.styles.scss'
 
 class SignUp extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
 
     this.state = {
       displayName: '',
@@ -21,9 +21,7 @@ class SignUp extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault()
-
     const { displayName, email, password, confirmPassword } = this.state()
-
 
     if (password !== confirmPassword) {
       alert("passwords don't match")
