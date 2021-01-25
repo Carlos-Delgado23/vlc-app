647a3a7e763a00a4b81d6635beeed212cc083a25
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-01-24 21:23:37 -0600

    routing and selector for collection

--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-import CollectionItem from '../../components/checkout-item/checkoutItem'
+import { connect } from 'react-redux'
+
+import { selectCollection } from '../../redux/shop/shopSelector'
 
 import './collection.styles.scss'
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className="collection-page">
     <h2>Collection PAGE</h2>
   </div>
 )
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
