af23f151bdc8c30e21c1fae61188a6568514e467
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-01-24 21:10:31 -0600

    improving naming of component

---
+++ src/pages/collection/collection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import CollectionItem from '../../components/checkout-item/checkoutItem'
+
+import './collection.styles.scss'
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>Collection PAGE</h2>
+  </div>
+)
+
+export default CollectionPage
