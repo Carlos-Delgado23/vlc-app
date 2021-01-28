b97e3df47c517c4209a5e3aaf4b28797f04934f6
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-01-27 22:33:50 -0600

    updating dataflow

--- src/components/collectionsOverview/collectionsOverview.js
+++ src/components/collectionsOverview/collectionsOverview.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import PreviewCollection from '../preview-collection/PreviewCollection'
-import { selectCollections } from '../../redux/shop/shopSelector'
+import { selectCollectionsForPreview } from '../../redux/shop/shopSelector'
 
 import './collectionsOverview.styles.scss'
 
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 
