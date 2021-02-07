6b290486d29198999c9929a9d31dd27d0dcbc807
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-02-06 22:25:14 -0600

    Cart item conv to a styled comp

--- src/components/cartIcon/cartIcon.styles.js
+++ src/components/cartIcon/cartIcon.styles.js
@@ -10,16 +10,16 @@ export const CartContainer = styled.div`
   align-items: center;
   justify-content: center;
   cursor: pointer;
-`;
+`
 
 export const ShoppingIcon = styled(ShoppingIconSVG)`
   width: 24px;
   height: 24px;
-`;
+`
 
 export const ItemCountContainer = styled.span`
   position: absolute;
   font-size: 10px;
   font-weight: bold;
   bottom: 12px;
-`;
+`
--- src/components/cartItem/cartItem.js
+++ src/components/cartItem/cartItem.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
-import './cartItem.styles.scss'
+import {
+  CartItemContainer,
+  ItemDetailsContainer,
+  CartItemImage
+} from './cartItem.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <div className="cart-item">
-    <img src={imageUrl} alt='item' />
-    <div className="item-details">
+  <CartItemContainer>
+    <CartItemImage src={imageUrl} alt='item' />
+    <ItemDetailsContainer>
       <span className='name'>{name}</span>
       <span className='price'>{quantity} x ${price}</span>
-    </div>
-  </div>
+    </ItemDetailsContainer>
+  </CartItemContainer>
 )
 
 export default CartItem
