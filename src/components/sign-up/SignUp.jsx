49240661c67ceb13af1694cdcbad3699033fb956
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-02-11 22:51:41 -0600

    sign up style conv

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -5,7 +5,7 @@ import CustomButton from '../custom-button/CustomButton'
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
 
-import './SignUp.styles.scss'
+import { SignUpContainer, SignUpTitle } from './SignUp.styles'
 
 class SignUp extends React.Component {
   constructor() {
@@ -54,8 +54,8 @@ class SignUp extends React.Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state
     return (
-      <div className="sign-up">
-        <h2 className="title">I do not have an account</h2>
+      <SignUpContainer>
+        <SignUpTitle>I do not have an account</SignUpTitle>
         <span>Sign up with your email and password</span>
 
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
@@ -93,7 +93,7 @@ class SignUp extends React.Component {
           />
           <CustomButton type='submit'>Sign Up</CustomButton>
         </form>
-      </div>
+      </SignUpContainer>
     )
   }
 }
