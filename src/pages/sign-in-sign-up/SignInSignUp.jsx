6c21dac1436113a6cc4264c614bfb09772acb8d2
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-02-13 22:57:38 -0600

    SignIn and SignUp style conv

--- src/pages/sign-in-sign-up/SignInSignUp.jsx
+++ src/pages/sign-in-sign-up/SignInSignUp.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import SignIn from '../../components/sign-in/SignIn'
 import SignUp from '../../components/sign-up/SignUp'
 
-import './SignInSignUp.styles.scss'
+import { SignInAndSignUpContainer } from './SignInSignUp.styles'
 
 const SignInSignUp = () => {
   return (
-    <div className='sign-in-and-sing-up'>
+    <SignInAndSignUpContainer>
       <SignIn />
       <SignUp />
-    </div>
+    </SignInAndSignUpContainer>
   )
 }
 
