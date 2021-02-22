b1ef3a8dc1061d602cb11687490b3b13a0675ad2
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2021-02-21 23:41:13 -0600

    moving shop data to app

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,13 +4,28 @@ import { Route } from 'react-router-dom'
 import CollectionsOverview from '../../components/collectionsOverview/collectionsOverview'
 import CollectionPage from '../collection/collection'
 
-
-const Shop = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-)
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
 
 
-export default Shop
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections')
+
+    collectionRef.onSnapshot(async snapshot => {
+      convertCollectionsSnapshotToMap(snapshot)
+    })
+  }
+  render() {
+    const { mactch } = this.props
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    )
+  }
+}
+
+
+export default ShopPage
